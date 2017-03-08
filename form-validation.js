58298d776997594c152d8fc2ba201493ee949192
Unknown <agarcia318@mail.csuchico.edu>
2017-03-07 23:57:52 -0800

    Validation fixed

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,37 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  var userEntered = document.getElementById("user").innerHTML = txt.length;
+
+function validate("user"){
+
+  if (txt.length >= 6) {
+
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else {
+
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+}
+
+function validate("pass"){
+  if(passEntered != "password")   {
+
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
+
+  else {
 
   //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
 }
